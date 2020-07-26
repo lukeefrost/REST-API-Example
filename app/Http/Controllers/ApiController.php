e36c9336dd5f704bbc7d085afd976692ecf9febc
lukelliotfrost <luke@radio.co>
2020-07-26 20:40:02 +0100

    Added API Controller for Student data as well as migration and routes for the application.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getAllStudents()
+    {
+
+    }
+
+    public function createStudent(Request $request)
+    {
+
+    }
+
+    public function getStudent($id)
+    {
+
+    }
+
+    public function updateStudent(Request $request)
+    {
+
+    }
+
+    public function deleteStudent($id)
+    {
+
+    }
+}
