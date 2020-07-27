7c680a23a9c8cfcc06ae91aba4ee9ac7f0fc54de
lukelliotfrost <luke@radio.co>
2020-07-27 18:50:19 +0100

    Added functionality to add a student record.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class ApiController extends Controller
 {
@@ -13,7 +14,14 @@ class ApiController extends Controller
 
     public function createStudent(Request $request)
     {
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
 
+        return response()->json([
+          "message" => "Student Record created"
+        ], 201);
     }
 
     public function getStudent($id)
