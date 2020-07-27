d4485c4b1d3d7ce38079edabd551107fec38c26c
lukelliotfrost <luke@radio.co>
2020-07-27 21:59:36 +0100

    Added functionality to update existing records

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -37,9 +37,23 @@ class ApiController extends Controller
         }
     }
 
-    public function updateStudent(Request $request)
+    public function updateStudent(Request $request, $id)
     {
+        if(Student::where('id', $id)->exists())
+        {
+          $student = Student::find($id);
+          $student->name = is_null($request->name) ? $student->name : $request->name; // Checks if name is null, if it isn't null the request value is passed. If it is null, null is passed as the value
+          $student->course = is_null($request->course) ? $student->course : $request->course; // Checks if course is null, if it isn't null the request value is passed. If it is null, null is passed as the value
+          $student->save();
 
+          return response()->json([
+            "message" => "Record updated successfully"
+          ], 200);
+        } else {
+          return response()->json([
+            "message" => "Student not found"
+          ], 404);
+        }
     }
 
     public function deleteStudent($id)
